97468cde7f2e6cd1ab8be6e1138499e671764f79
i-douk <101892232+i-douk@users.noreply.github.com>
2024-10-28 22:27:26 +0100

    add post user endpoint

--- backend/controllers/users/users.ts
+++ backend/controllers/users/users.ts
@@ -1,23 +1,43 @@
 const usersRouter = require('express').Router()
 import  User  from '../../models/user';
-import Podcaster from '../../models/podcaster';
-import Podcast from '../../models/podcast';
+// import Podcaster from '../../models/podcaster';
+// import Podcast from '../../models/podcast';
 // import { sequelize } from '../../util/db';
 
-usersRouter.get('/', async (_req: any, res: { json: (arg0: User[]) => void; }) => {
-  
+
+//get all users , their podcasters subscription and their followed podcasts
+usersRouter.get('/', async (_req, res) => {
+  try {
     const users = await User.findAll({
-        include: [{
-            model: Podcaster,
-            attributes: { exclude: ['userId'] }
-          },
-          {
-            model: Podcast, 
-            attributes: ['name', 'description'], 
-          }
-       ]
-      });
-      res.json(users);
-})
+    //   include: [
+    //   {
+    //     model: Podcast,
+    //     as: 'follows',
+    //     attributes: { exclude: [''] },      
+    //   },
+    //   {
+    //     model: Podcaster,
+    //     as: 'subscriptions', 
+    //     attributes: { exclude: [''] },     
+    //   }
+    // ]
+    });
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+//create a new user
+usersRouter.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({ error: 'Failed to create user' });
+  }
+});
 
 export default usersRouter;
