44685223bdcddcdeb68ed30a9b7aaeebebfea188
i-douk <101892232+i-douk@users.noreply.github.com>
2024-10-30 01:13:06 +0100

    add user update and get user endpoints

--- backend/controllers/users/users.ts
+++ backend/controllers/users/users.ts
@@ -1,5 +1,6 @@
 const usersRouter = require('express').Router()
 import  User  from '../../models/user';
+import { tokenExtractor } from '../../util/middleware';
 // import Podcaster from '../../models/podcaster';
 // import Podcast from '../../models/podcast';
 // import { sequelize } from '../../util/db';
@@ -8,20 +9,7 @@ import  User  from '../../models/user';
 //get all users , their podcasters subscription and their followed podcasts
 usersRouter.get('/', async (_req, res) => {
   try {
-    const users = await User.findAll({
-    //   include: [
-    //   {
-    //     model: Podcast,
-    //     as: 'follows',
-    //     attributes: { exclude: [''] },      
-    //   },
-    //   {
-    //     model: Podcaster,
-    //     as: 'subscriptions', 
-    //     attributes: { exclude: [''] },     
-    //   }
-    // ]
-    });
+    const users = await User.findAll({});
     res.json(users);
   } catch (error) {
     console.error(error);
@@ -43,6 +31,40 @@ usersRouter.post('/', async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(400).json({ error: 'Failed to create user' });
+  }
+});
+
+// Get a user by username
+usersRouter.get('/:username', async (req, res) => {  
+  try {
+    const user = await User.findOne({ 
+      where: { username: req.params.username }
+    });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+// Update a user's name
+usersRouter.put('/:username', tokenExtractor, async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { username: req.params.username } });
+    if (user) {
+      user.name = req.body.name;
+      await user.save();
+      res.json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
   }
 });
 
