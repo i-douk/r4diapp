0627aca7d5de314136caad91c55293b67b13c7a2
i-douk <101892232+i-douk@users.noreply.github.com>
2024-10-31 10:05:16 +0100

    add subcount and follow count migration

---
+++ backend/migrations/20241031_00_add_followcount_subscriptioncount.ts
@@ -0,0 +1,20 @@
+import { DataTypes } from 'sequelize'
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.addColumn('podcasters', 'subscriptioncount', {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      allowNull : false
+    })
+    await queryInterface.addColumn('podcasts', 'followcount', {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      allowNull : false
+    })
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.removeColumn('podcasters', 'subscriptioncount')
+    await queryInterface.removeColumn('podcasts', 'followcount')
+  },
+}
